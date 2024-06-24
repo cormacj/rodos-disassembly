a15601269347c551edabbd613903b87509942d2b
Cormac McGaughey <cormac.mcgaughey@gmail.com>
2024-06-23 21:06:45 -0700

    Update known bugs for v2.21; move to v2.22. Validation work

---
+++ rodos-222/debug_off.asm
@@ -0,0 +1,6 @@
+;Define a debug variable
+;Used to create an additonal command for workspace reporting
+;0=No debug
+;1=Debug
+
+DEBUG: equ 0
---
+++ rodos-222/validate-size.sh
@@ -0,0 +1,9 @@
+    #!/usr/bin/env bash
+destsize=`ls -l $1|awk '{print $5}'`
+if [ "16384" != $destsize ]; then
+  echo "Error! File size is not 16384 - may not validate as a ROM"
+  echo "File size was $destsize"
+  exit 255
+else
+  echo "File is valid!"
+fi
